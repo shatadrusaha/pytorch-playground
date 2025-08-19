083d179963a32b8460f6dfb05dfd3f8ae13d623c
Shaz <shatadrusaha@gmail.com>
2025-08-19 20:10:27 +0100

    initial commit

---
+++ src/i-pytorch-intro.py
@@ -0,0 +1,6 @@
+
+# https://formulae.brew.sh/formula/gh#default
+# https://www.youtube.com/watch?v=5rTwOt9Qgik
+
+# gh auth login
+# gh repo create pytorch-playground --public --source=. --remote=origin --push
